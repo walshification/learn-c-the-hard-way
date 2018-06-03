85d1e35c81ed2381820ad4b05489546a954c6b5d
Christopher Walsh <walshification@gmail.com>
2018-06-03 15:01:01 -0500

    completes ex1

--- ex1.c
+++ ex1.c
@@ -1,9 +1,23 @@
-#include <stdio.h>
+#include <stdio.h> // how to import another file's behavior into this one
 
+/* This is a multi-line comment.
+Any number of characters can go here. */
+int main(int argc, char *argv[]) // The main function that gets this whole show running
+// argc is an integer that represents the count of arguments passed into main
+// char *argv[] is an array of strings for arguments
+{ // the start of the function block
 
-int main(int argc, char *argv[])
-{
-    puts("Hello world.");
+    /* variable declaration and assignment
+    in C, all statements (except for logic) must end with a ;
+    in English, the below line reads:
+    "create an integer referred to as 'distance' and assign it the value 100." */
+    int distance = 100;
 
+    // this is a one-line comment
+    // printf sends a string and any number of variables to stdout
+    printf("You are %d miles away.\n", distance);
+
+    // our last statement, which returns a code to our UNIX-based OS that tells
+    // it how the execution of the program went. 0 means no errors.
     return 0;
-}
+} // the closing of our execution block
