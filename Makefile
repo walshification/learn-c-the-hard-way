e83fc639f4e6be93e5000ec962ed56588be27c47
Christopher Walsh <walshification@gmail.com>
2018-06-03 17:12:03 -0500

    adds a dummy ex2.c

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CFLAGS=-Wall -g
 
-all: ex1
+all: ex1 ex2
 
 clean:
-	rm -f ex1
+	rm -f ex1 ex2
 
 .PHONY: all clean
---
+++ ex2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("See the sweet new Makefile.\n");
+    return 0;
+}
