67a54bd6734fb183a4c28ccb2b2dc52c85baae67
Christopher Walsh <walshification@gmail.com>
2018-06-04 07:31:41 -0500

    fixes if to only print user arguments

--- ex8.c
+++ ex8.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     } else if (argc > 1 && argc < 4) {
         printf("Here are your arguments:\n");
 
-        for (i = 0; i < argc; i++) {
+        for (i = 1; i < argc; i++) {
             printf("%s ", argv[i]);
         }
         printf("\n");
